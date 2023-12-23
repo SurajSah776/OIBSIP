0c2d3fb767adeb812ddd71cbbd894362dcaeb86f
SurajSah776 <surajkumarsah2060@gmail.com>
2023-12-23 12:35:39 +0530

    JavaScript Logic Upload for Temperature Converter Website

--- Task-3 Temperature Converter Website/script.js
+++ Task-3 Temperature Converter Website/script.js
@@ -1,24 +1,91 @@
-let celsius = document.getElementById("celsius");
-let fahrenheit = document.getElementById("fahrenheit");
-let kelvin = document.getElementById("kelvin");
-celsius.oninput = function () {
-    let f = (parseFloat(celsius.value) * 9) / 5 + 32;
-    fahrenheit.value = parseFloat(f.toFixed(2));
+let initial, final;
 
-    let k = parseFloat(celsius.value) + 273.15;
-    kelvin.value = parseFloat(k.toFixed(2));
-};
-fahrenheit.oninput = function () {
-    let c = ((parseFloat(fahrenheit.value) - 32) * 5) / 9;
-    celsius.value = parseFloat(c.toFixed(2));
+function getInfo() {
+    const initial_text = document.getElementById("initial");
+    initial = initial_text.options[initial_text.selectedIndex].value;
 
-    let k = ((parseFloat(fahrenheit.value) - 32) * 5) / 9 + 273.15;
-    kelvin.value = parseFloat(k.toFixed(2));
-};
-kelvin.oninput = function () {
-    let f = ((parseFloat(kelvin.value) - 273.15) * 9) / 5 + 32;
-    fahrenheit.value = parseFloat(f.toFixed(2));
+    const final_text = document.getElementById("final");
+    final = final_text.options[final_text.selectedIndex].value;
+}
 
-    let c = parseFloat(kelvin.value) - 273.15;
-    celsius.value = parseFloat(c.toFixed(2));
-};
+const input = document.getElementById("input");
+const output = document.getElementById("output");
+const convertButton = document.getElementById("convert-btn");
+
+
+// Functions to convert temperature from one to another
+const celsiusToFahrenheit = () => {
+    getInfo();
+    const fahrenheit = (input.value * 9 / 5) + 32;
+    output.innerText = fahrenheit.toFixed(3) + " °F";
+}
+
+const fahrenheitToCelsius = () => {
+    getInfo();
+    const celsius = (input.value - 32) * 5 / 9;
+    output.innerText = celsius.toFixed(3) + " °C";
+}
+
+const celsiusToKelvin = () => {
+    getInfo();
+    const kelvin = parseFloat(input.value) + 273.15;
+    output.innerText = kelvin.toFixed(3) + " K";
+}
+
+const kelvinToCelsius = () => {
+    getInfo();
+    const celsius = parseFloat(input.value) - 273.15;
+    output.innerText = celsius.toFixed(3) + " °C";
+}
+
+const fahrenheitToKelvin = () => {
+    getInfo();
+    const kelvin = (input.value - 32) * 5 / 9 + 273.15;
+    output.innerText = kelvin.toFixed(3) + " K";
+}
+
+const kelvinToFahrenheit = () => {
+    getInfo();
+    const fahrenheit = (input.value - 273.15) * 9 / 5 + 32;
+    output.innerText = fahrenheit.toFixed(3) + " °F";
+}
+
+// Main Method
+function mainMethod() {
+    convertButton.addEventListener("click", () => {
+        getInfo();
+        if (initial === "Celsius" && final === "Fahrenheit") {
+            celsiusToFahrenheit();
+        }
+
+        else if (initial === "Fahrenheit" && final === "Celsius") {
+            fahrenheitToCelsius();
+        }
+
+        else if (initial === "Celsius" && final === "Kelvin") {
+            celsiusToKelvin();
+        }
+
+        else if (initial === "Kelvin" && final === "Celsius") {
+            kelvinToCelsius();
+        }
+
+        else if (initial === "Fahrenheit" && final === "Kelvin") {
+            fahrenheitToKelvin();
+        }
+
+        else if (initial === "Kelvin" && final === "Fahrenheit") {
+            kelvinToFahrenheit();
+        }
+
+        else {
+            output.innerText = input.value;
+        }
+    });
+}
+
+mainMethod();
+
+
+
+mainMethod();
