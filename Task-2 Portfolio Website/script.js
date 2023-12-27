1bb2595f8b16ec33f612270cd3863a54dc016680
SurajSah776 <surajkumarsah2060@gmail.com>
2023-12-27 13:12:58 +0530

    Finalizing the code

---
+++ Task-2 Portfolio Website/script.js
@@ -0,0 +1,13 @@
+// Script for Smooth Scroll Animation
+
+document.addEventListener('DOMContentLoaded', function () {
+    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+        anchor.addEventListener('click', function (e) {
+            e.preventDefault();
+
+            document.querySelector(this.getAttribute('href')).scrollIntoView({
+                behavior: 'smooth'
+            });
+        });
+    });
+});
