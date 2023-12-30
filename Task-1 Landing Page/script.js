c8d244ea95ec7b421e6f924c2afc398c01859449
SurajSah776 <surajkumarsah2060@gmail.com>
2023-12-30 17:00:23 +0530

    Updated Every Sections with CSS

--- Task-1 Landing Page/script.js
+++ Task-1 Landing Page/script.js
@@ -18,3 +18,42 @@ document.addEventListener('DOMContentLoaded', function () {
         });
     });
 });
+
+
+// For Home Section Carousel
+const slides = document.getElementsByClassName("slide");
+let slideIndex = 0;
+
+function showSlide(index) {
+    if (index >= slides.length) {
+        slideIndex = 0;
+    } else if (index < 0) {
+        slideIndex = slides.length - 1;
+    } else {
+        slideIndex = index;
+    }
+
+    for (let slide of slides) {
+        slide.style.display = "none";
+    }
+
+    slides[slideIndex].style.display = "block";
+}
+// Show the first slide immediately
+showSlide(slideIndex);
+
+setTimeout(() => {
+    const slideInterval = setInterval(() => {
+        slideIndex++;
+        showSlide(slideIndex);
+    }, 2500);
+
+    function controller(x) {
+        clearInterval(slideInterval);
+        slideIndex += x;
+        showSlide(slideIndex);
+    }
+
+    document.querySelector('.prev').addEventListener('click', () => controller(-1));
+    document.querySelector('.next').addEventListener('click', () => controller(1));
+}, 2500);
